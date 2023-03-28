f251792a72ddd333de4f96b05d830a6f4f25c404
PiggyChu620 <ytchu620@gmail.com>
2023-03-28 13:28:17 +0800

    202230328

--- src/ec620/EC620JavaMod.java
+++ src/ec620/EC620JavaMod.java
@@ -33,7 +33,7 @@ public class EC620JavaMod extends Mod
     {
 
         //listen for game load event
-        /*Events.on(ClientLoadEvent.class, e -> {
+        Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 Dialog.DialogStyle dd=new Dialog.DialogStyle();
@@ -41,20 +41,20 @@ public class EC620JavaMod extends Mod
                 dd.titleFont= Fonts.def;
                 dd.titleFontColor=new Color(1,0,0);
                 BaseDialog dialog = new BaseDialog("Random Planet", dd);
-                dialog.cont.add("Welcome to Random Planet v0.2",Color.green,1.2f).row();
-                dialog.cont.add("This mod randomly generate a planet and its sectors, no presets, nothing.",Color.cyan).row();
-                dialog.cont.add("If you like some varieties and changes, then this mod is for you.",Color.cyan).row();
-                dialog.cont.add("I combined both Serpulo and Erekir techs together as best as I could,",Color.cyan).row();
-                dialog.cont.add("plus some of my own creations, in general, it's a hard game,",Color.cyan).row();
-                dialog.cont.add("some of the sectors (especially Eradication maps) might even be unbeatable,",Color.cyan).row();
-                dialog.cont.add("so if you're a newbie, I recommend you to play the original games",Color.cyan).row();
-                dialog.cont.add(" and get familiar with the contents first.",Color.cyan).row();
+                dialog.cont.add("Welcome to Random Planet v0.2.1",Color.green,1.2f).row();
+                dialog.cont.add("You need to turn the phone to landscape in order to see the",Color.cyan).row();
+                dialog.cont.add("setting info, I can not figure out how to make it wrap, sorry.",Color.cyan).row();
+//                dialog.cont.add("I combined both Serpulo and Erekir techs together as best as I could,",Color.cyan).row();
+//                dialog.cont.add("plus some of my own creations, in general, it's a hard game,",Color.cyan).row();
+//                dialog.cont.add("some of the sectors (especially Eradication maps) might even be unbeatable,",Color.cyan).row();
+//                dialog.cont.add("so if you're a newbie, I recommend you to play the original games",Color.cyan).row();
+//                dialog.cont.add(" and get familiar with the contents first.",Color.cyan).row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 //dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("OK", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
-        });*/
+        });
         /*Events.on(PlayEvent.class, event -> {
             //Log.info("Listening to PlayEvent: Sector "+Vars.state.rules.sector.id);
             if (Vars.state.rules.sector != null && Vars.state.rules.sector.id == 0)
